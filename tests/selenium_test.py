ff8fa180a1eff46ca753b77629e1dba86c6b268d
han54369 <82426808+han54369@users.noreply.github.com>
2026-06-23 04:53:25 +0900

    Update selenium_test.py

--- tests/selenium_test.py
+++ tests/selenium_test.py
@@ -1,18 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.chrome.service import Service
 
 def test_google_title():
-    # Docker 환경(GUI 화면 없음)을 위한 Chrome Headless 설정
     chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
+    # Alpine 리눅스에 설치된 Chromium 실제 경로 지정
+    chrome_options.binary_location = '/usr/bin/chromium-browser'
     
-    # 드라이버 실행 및 웹페이지 접속
-    driver = webdriver.Chrome(options=chrome_options)
+    chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--no-sandbox')
+    chrome_options.add_argument('--disable-dev-shm-usage')
+    
+    # Alpine 리눅스에 설치된 Chromedriver 실제 경로 지정
+    service = Service('/usr/bin/chromedriver')
+    
+    # 드라이버 실행 시 service와 options 함께 전달
+    driver = webdriver.Chrome(service=service, options=chrome_options)
     
     try:
-        driver.get("https://www.google.com")
-        assert "Google" in driver.title
+        driver.get('https://www.google.com')
+        assert 'Google' in driver.title
     finally:
         driver.quit()
